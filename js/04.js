78f02a52b107a9b17728439e30ba936a1a9d780b
Usiel-03A7 <shebastyan@gmail.com>
2024-07-22 13:36:15 -0600

    makea new knoeledge of objects and destructuring

---
+++ js/04.js
@@ -0,0 +1,30 @@
+// Objets
+// const product = {
+//     name: "Pickle",
+//     price: 300,
+//     avaliable: true
+// }
+// console.log(product);
+// console.table(product)
+
+// // Destructuing
+// const car = {
+//     model: "Z8",
+//     companny: "Tesla",
+//     price: 323443242,
+//     year: 2019
+// }
+
+// const { model, companny } = car
+// console.log(model);
+// console.log(companny);
+
+// Object Literal Enhacemet
+const auth = true;
+const name = "Pepe"
+
+const user = {
+    auth,
+    name
+}
+console.table(user)
