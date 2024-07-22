4984b96741e156d14141db9315cd8b76bf91a6d2
Usiel-03A7 <shebastyan@gmail.com>
2024-07-22 11:59:05 -0600

    see the difference between data type

---
+++ js/03.js
@@ -0,0 +1,58 @@
+/**
+ * JS data type 
+ * undefined
+ * let client 
+ * console.log(client)
+ * console.log(typeof client)
+ */
+
+/**
+ * String of text 
+ * const product = "monitor 49'' "
+ * console.log(product)
+ * console.log(typeof product)
+ */
+
+/**
+ * Numbers 
+ * const number = 22
+ * const number2 = 23
+ * const number3 = 3
+ * console.log(number)
+ * console.log(number2)
+ * console.log(number3)
+ * console.log(typeof number)
+ * console.log(typeof number2)
+ * console.log(typeof number3)
+ */
+
+/**
+ * BigInt 
+ * const bigNumber = BigInt(234523543234235324234)
+ * console.log(typeof bigNumber)
+ */
+
+/**
+ * Boolean
+ * const priceoff = true
+ * console.log(priceoff);
+ */
+
+/**
+ * Null 
+ * const priceoff = null
+ * console.log(priceoff)
+ * 
+ * Diference between null and undefined
+ * 
+ * const price
+ * console.log(price)
+ */
+
+/**
+ * Symbol 
+ * const firstSymbol = Symbol(30)
+ * const secondSymbol = Symbol(30)
+ * console.log(fistSymbol === secondSymbol)
+ * Symbol is only one in the code 
+ */
