0a10afd998dffc45c649bc6afb1338e4f4eee6ed
Usiel-03A7 <shebastyan@gmail.com>
2024-07-22 11:26:21 -0600

    start with the variable let

--- js/01.js
+++ js/01.js
@@ -1,1 +1,14 @@
-console.log("hola mundo");
+/**
+ * diference between var, let and const
+ * 
+ * let variable
+ */
+let client = "Usiel"
+
+console.log(client);
+
+let price;
+
+price = 20;
+
+console.log(price);
