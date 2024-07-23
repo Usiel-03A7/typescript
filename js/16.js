abda018444f3460b11f0cd6e786dc6402e01ce30
Usiel-03A7 <shebastyan@gmail.com>
2024-07-23 13:43:06 -0600

    Learn functions and return each

---
+++ js/16.js
@@ -0,0 +1,17 @@
+//retunr values with the functions seend previously
+
+//function expresion
+
+// const sum = function (a, b) {
+//     return a + b
+// }
+
+//function declaration
+// function sum    (a, b) {
+//     return a + b
+// }
+
+// arrow function 
+const sum = (a, b) => console.log(a + b);
+
+sum(23, 63)
