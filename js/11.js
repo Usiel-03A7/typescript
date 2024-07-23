31e68caee035c1cce56e2ca6a483c13b89dc9534
Usiel-03A7 <shebastyan@gmail.com>
2024-07-23 12:11:20 -0600

    learn destructuring to arrays

---
+++ js/11.js
@@ -0,0 +1,9 @@
+const tecnology = ["Html", "Css", "Nodejs", "Reactjs", "Typescript", "Usiel"]
+
+const react = tecnology[4]
+
+//Destruturing arrays
+
+const [, , , , typescrip] = tecnology;
+
+console.log(typescrip);
