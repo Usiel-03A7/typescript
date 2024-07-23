a4bfdccbe3c10f909ac5c556da1d52c4bb9e09ee
Usiel-03A7 <shebastyan@gmail.com>
2024-07-23 11:10:01 -0600

    learn template strings and contactenation

---
+++ js/08.js
@@ -0,0 +1,8 @@
+//Template String and concatenation
+
+const product = "Tablet 12 inches"
+const price = 999
+const brand = "Samsung"
+
+console.log("El producto es: " + product + " Precio:" + price + " marca:" + brand);
+console.log(`El producto es ${product} Precio:  ${price} Marca: ${brand}`);
