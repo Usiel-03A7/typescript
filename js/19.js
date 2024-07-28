3eb636a19783a62b1daa93684237f38acd6987a2
Usiel-03A7 <shebastyan@gmail.com>
2024-07-28 12:32:03 -0600

    Learn normal equals and strict equals

---
+++ js/19.js
@@ -0,0 +1,8 @@
+const number1 = 20
+const number2 = "20"
+//the == check only the number and === check number and data type
+if (number1 == number2) {
+    console.log('This is equals');
+} else {
+    console.log("Tis is not equals");
+}
