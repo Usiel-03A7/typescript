455c8a74e340839f016d3a17967fb37496153288
Usiel-03A7 <shebastyan@gmail.com>
2024-07-28 12:20:07 -0600

    Lear the conditionals operators

---
+++ js/18.js
@@ -0,0 +1,21 @@
+const avaliable = 34
+const cash = 34
+const auth = false
+
+if (avaliable >= cash) {
+    console.log("Rigth , you can to take the cash");
+} else {
+    console.log("U're broke bro.");
+}
+
+
+/**
+ * > Greater-than 
+ * < Less-than 
+ * >= Greater-than equals
+ * <= Less=than equals 
+ * == Equals 
+ * === Equals strict
+ * !== Unquals or Different
+ * 
+ */
